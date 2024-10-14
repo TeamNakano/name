a3c738ca11b9c85dec6b162bbca5e05149f7b504
TeamNakano <tiktokclips269@gmail.com>
2024-10-13 23:24:04 -0300

    Update bots-serbot.js

--- plugins/bots-serbot.js
+++ plugins/bots-serbot.js
@@ -1,4 +1,4 @@
-import { DisconnectReason, useMultiFileAuthState, fetchLatestBaileysVersion, makeCacheableSignalKeyStore, jidNormalizedUser } from '@whiskeysockets/baileys'
+/*import { DisconnectReason, useMultiFileAuthState, fetchLatestBaileysVersion, makeCacheableSignalKeyStore, jidNormalizedUser } from '@whiskeysockets/baileys'
 import qrcode from 'qrcode'
 import fs from 'fs'
 import pino from 'pino'
@@ -146,4 +146,4 @@ handler.command = ["serbot", "jadibot", "qr", "botclone"]
 export default handler
 function sleep(ms) {
 return new Promise(resolve => setTimeout(resolve, ms))
-}
+}*/
