567bf32867c96cea771d9152d769fb23d9c19fff
Oscar66266 <oscarabrahamcabreramarin098@gmail.com>
2024-10-17 22:08:40 -0500

    Update nsfw-xnxxdl.js

--- plugins/nsfw-xnxxdl.js
+++ plugins/nsfw-xnxxdl.js
@@ -1,22 +1,32 @@
-import Starlights from '@StarlightsTeam/Scraper'
+import fg from 'api-dylux'
 
 let handler = async (m, { conn, args, command, usedPrefix }) => {
-if (!global.db.data.chats[m.chat].nsfw) return conn.reply(m.chat, `🚩 El grupo no admite contenido *Nsfw.*\n\n> Para activarlo un *Administrador* debe usar el comando */nsfw on*`, m, rcanal)
-if (!args[0]) return conn.reply(m.chat, `🚩 Ingresa el enlace del vídeo de Xnxx*`, m, rcanal)
+    if (!args[0]) return conn.reply(m.chat, `Ingresa un enlace de xnxx`, m)
 
-let user = global.db.data.users[m.sender]
-await m.react('🕓')
-try {
-let { title, dl_url } = await Starlights.xnxxdl(args[0])
-await conn.sendFile(m.chat, dl_url, title + '.mp4', `*» Título* : ${title}`, m, false, { asDocument: user.useDocument })
-await m.react('✅')
-} catch {
-await m.react('✖️')
-}}
+    try {
+        
+        await m.react('🕗')
+
+        let { title, quality, duration, size, thumb, url_dl: dl } = await fg.xnxxdl(args[0])
+        let txt = `*Nombre:* ${title}
+*Tamaño:* ${size}
+*Calidad:* ${quality}
+*Duracion:* ${duration}`.trim()
+
+        
+        await conn.sendFile(m.chat, dl, title + '.mp4', ``, m, false, { asDocument: true }) 
+        
+        
+        await m.react('✅')
+    } catch (error) {
+        console.log(error)
+        
+        await m.react('✖️')
+    }
+}
+
 handler.tags = ['nsfw', 'downloader']
 handler.help = ['xnxxdl *<url>*']
 handler.command = ['xnxxdl']
-//handler.limit = 200
-handler.group = true 
-handler.register = true 
+
 export default handler
