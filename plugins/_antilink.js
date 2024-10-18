fda573ed0fbae6dcf10891fe6d416307e2de33f7
Oscar66266 <oscarabrahamcabreramarin098@gmail.com>
2024-10-17 22:26:58 -0500

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,23 +1,42 @@
-const linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
+let linkRegex = /(https?:\/\/(?:www\.)?(?:t\.me|telegram\.me|whatsapp\.com)\/\S+)|(https?:\/\/chat\.whatsapp\.com\/\S+)|(https?:\/\/whatsapp\.com\/channel\/\S+)/i
 
-export async function before(m, {conn, isAdmin, isBotAdmin }) {
+export async function before(m, { isAdmin, isBotAdmin }) {
     if (m.isBaileys && m.fromMe)
         return !0
     if (!m.isGroup) return !1
     let chat = global.db.data.chats[m.chat]
+    let delet = m.key.participant
+    let bang = m.key.id
     let bot = global.db.data.settings[this.user.jid] || {}
     const isGroupLink = linkRegex.exec(m.text)
+    const grupo = `https://chat.whatsapp.com`
 
+    
+    if (isAdmin && chat.antiLink && m.text.includes(grupo)) return conn.sendMessage(m.chat, { react: { text: "✅", key: m.key } })
+
+    
     if (chat.antiLink && isGroupLink && !isAdmin) {
         if (isBotAdmin) {
             const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
-            if (m.text.includes(linkThisGroup)) return !0
+            if (m.text.includes(linkThisGroup)) return conn.sendMessage(m.chat, { react: { text: "✅", key: m.key } })
         }
-        await conn.reply(m.chat, `🚩 No permitimos enlaces de otros grupos, lo siento *@${m.sender.split('@')[0]}* serás expulsado del grupo ${isBotAdmin ? '' : '\n\nNo soy admin así que no te puedo expulsar :"v'}`, null, { mentions: [m.sender] } )
-        if (isBotAdmin && chat.antiLink) {
-        	await conn.sendMessage(m.chat, { delete: m.key })
+
+        await conn.sendMessage(m.chat, { react: { text: "❌", key: m.key } })
+
+        
+        if (!isBotAdmin) {
+            return conn.reply(m.chat, `❌ *Necesito ser administradora para poder eliminar a usuarios que compartan enlaces.*`, m)
+        }
+
+        
+        if (isBotAdmin) {
+            await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet } })
             await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-        } else if (!chat.antiLink) return //m.reply('')
+
+            
+            let username = await this.getName(m.sender)
+            await conn.sendMessage(m.chat, { text: `🚫 *Elimine a ${username} por que el antilink esta activo.*` })
+        } else if (!bot.restrict) return
     }
     return !0
 }
