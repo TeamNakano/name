9114f3848d8d26c805a0e4fa09e02a8e063b2268
Oscar66266 <oscarabrahamcabreramarin098@gmail.com>
2024-10-19 19:47:15 -0500

    Update youtube-play.js

--- plugins/youtube-play.js
+++ plugins/youtube-play.js
@@ -1,38 +1,104 @@
-import fetch from "node-fetch";
-import yts from "yt-search";
+import { prepareWAMessageMedia, generateWAMessageFromContent } from '@whiskeysockets/baileys';
+import yts from 'yt-search';
 
-const handler = async (m, { text, usedPrefix, command, conn }) => {
-  try {
-    if (!text) {
-      throw m.reply("Ingresa una consulta de *YouTube*");
+const handler = async (m, { conn, text, usedPrefix, command, args }) => {
+    if (!args[0]) return m.reply(`*¿Qué quieres que busque, tu botsita Nakano?* 😘`);
+
+    await m.react('🕗');
+
+    const results = await yts(text);
+    const video = results.videos[0]; 
+
+    if (!video) {
+        await m.react('❌');
+        return m.reply(`❌ Lo siento, no encontré resultados para *${text}*. Intenta con otro término de búsqueda.`);
     }
-    let res = await yts(text);
-    let videoList = res.all;
-    if (videoList.length === 0) {
-      throw m.reply("No se encontraron resultados para la consulta");
-    }
-    let video = videoList[Math.floor(Math.random() * videoList.length)];
-    let texto = `_*Reproduciendo ${video.title}...*_`;
-    await conn.sendMessage(m.chat, { react: { text: `🎵`, key: m.key } });
-    await conn.sendMessage(m.chat, { image: { url: video.thumbnail }, caption: texto }, { quoted: m });
-    let apiUrl = `(link unavailable){encodeURIComponent(video.url)}&key=gojou`;
-    let result = await (await fetch(apiUrl)).json();
-    if (result.status && result.code === 200 && result.result && result.result.download_url) {
-      let audioUrl = result.result.download_url;
-      await conn.sendMessage(m.chat, { audio: { url: audioUrl }, mimetype: "audio/mp4" }, { quoted: m });
-    } else {
-      m.reply("No se encontró una respuesta de la API.");
-    }
-  } catch (error) {
-    console.error(error);
-    m.reply("Error interno, intenta más tarde.");
-  }
+
+    await m.react('✅');
+
+    
+    var mediaMessage = await prepareWAMessageMedia({ image: { url: video.thumbnail } }, { upload: conn.waUploadToServer });
+
+    
+    const videoInfo = `
+*💞 Título:* ${video.title}
+*📅 Creado:* ${video.ago}
+*🕗 Duración:* ${video.timestamp}
+*👁️ Vistas:* ${video.views.toLocaleString()}
+*💬 Link:* ${video.url}
+    `.trim();
+
+    
+    const interactiveMessage = {
+        body: {
+            text: videoInfo,
+        },
+        footer: { text: 'Powered by Nakano Team' },
+        header: {
+            title: `*Y T  -  D E S C A R G A S* 💞`,
+            hasMediaAttachment: true,
+            imageMessage: mediaMessage.imageMessage,
+        },
+        nativeFlowMessage: {
+            buttons: [
+                {
+                    name: 'single_select',
+                    buttonParamsJson: JSON.stringify({
+                        title: 'Selecciona una opcion',
+                        sections: [
+                            {
+                                title: video.title,
+                                rows: [
+                                    {
+                                        header: video.title,
+                                        title: 'MP3',
+                                        description: `Descargar MP3`,
+                                        id: `${usedPrefix}ytmp3 ${video.url}`
+                                    },
+                                    {
+                                        header: video.title,
+                                        title: 'MP3DOC',
+                                        description: `Descargar MP3DOC`,
+                                        id: `${usedPrefix}ytmp3doc ${video.url}`
+                                    },
+                                    {
+                                        header: video.title,
+                                        title: 'MP4',
+                                        description: `Descargar MP4`,
+                                        id: `${usedPrefix}ytmp4 ${video.url}`
+                                    },
+                                    {
+                                        header: video.title,
+                                        title: 'MP4DOC',
+                                        description: `Descargar MP4DOC`,
+                                        id: `${usedPrefix}ytmp4doc ${video.url}`
+                                    }
+                                ]
+                            }
+                        ]
+                    })
+                }
+            ],
+            messageParamsJson: ''
+        }
+    };
+
+    
+    let msg = generateWAMessageFromContent(m.chat, {
+        viewOnceMessage: {
+            message: {
+                interactiveMessage,
+            },
+        },
+    }, { userJid: conn.user.jid, quoted: m });
+
+    conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id });
 };
 
-handler.help = ["play <consulta>"];
-handler.tags = ["downloader"];
-handler.command = /^(play)$/i;
-handler.premium = false;
-handler.register = true;
+handler.help = ['play <texto>'];
+handler.tags = ['downloader'];
+handler.command = ['play'];
+handler.register = false;
+handler.group = false;
 
 export default handler;
