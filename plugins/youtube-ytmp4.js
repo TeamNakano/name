0a01a8bf3796e94ad4117648248eba43b210cba9
Oscar66266 <oscarabrahamcabreramarin098@gmail.com>
2024-10-22 08:50:11 -0500

    Update youtube-ytmp4.js

--- plugins/youtube-ytmp4.js
+++ plugins/youtube-ytmp4.js
@@ -1,58 +1,35 @@
+import axios from 'axios';
 
+let handler = async (m, { conn, command, args }) => {
+  const url = args[0];
+  if (!url) return conn.reply(m.chat, '💬 Ingresa la URL del video de YouTube junto al comando.', m);
+  
+  await m.react('🕓');
 
+  try {
+    
+    const response = await axios.get(`https://api.ryzendesu.vip/api/downloader/ytmp4?url=${encodeURIComponent(url)}`, {
+      headers: { accept: 'application/json' }
+    });
 
-import fetch from 'node-fetch';
+    const videoUrl = response.data.url;
 
-let handler = async (m, { conn, args }) => {
-    if (!args[0]) return conn.reply(m.chat, '🚩 Por favor, ingresa un enlace de YouTube.', m, rcanal);
-
-    await m.react('🕗');  
-
-    try {
-        let url = `https://widipe.com/download/ytdl?url=${encodeURIComponent(args[0])}`;
-        let response = await fetch(url);
-        let json = await response.json();
-
-        if (json.status && json.result && json.result.mp4) {
-            let { title, thumbnail, duration, views, mp4, size } = json.result;
-            let mp4Url = mp4;
-
-            
-            let sizeMB = parseFloat(size);
-
-            
-            if (sizeMB > 130) {
-                return conn.reply(m.chat, `🚩 El video es demasiado grande (${sizeMB} MB). El límite es 130 MB.`, m);
-            }
-
-            
-            let message = `
-*💞 Título:* ${title}
-*💞 Duración:* ${duration}
-*💞 Vistas:* ${views}
-*💞 Tamaño:* ${size}
-*💞 Enlace Original:* ${json.result.url}
-            `.trim();
-
-            
-            await conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', message, m);
-
-            
-            await conn.sendFile(m.chat, mp4Url, `${title}.mp4`, '', m, false, { mimetype: 'video/mp4' });
-
-            await m.react('✅'); 
-        } else {
-            await conn.reply(m.chat, '🚩 No se pudo obtener el archivo de video MP4.', m);
-            await m.react('❌');  
-        }
-    } catch (error) {
-        console.error(error);
-        await conn.reply(m.chat, '🚩 Ocurrió un error al procesar tu solicitud puede que el video sea muy pesado intente con .ytmp4doc.', m, rcanal);
-        await m.react('❌');  
+    if (videoUrl) {
+      
+      await conn.sendMessage(m.chat, { video: { url: videoUrl }, mimetype: 'video/mp4' }, { quoted: m });
+      await m.react('✅');
+    } else {
+      conn.reply(m.chat, '❌ No se encontró el enlace de descarga del video.', m);
     }
-};
+  } catch (error) {
+    console.error(error);
+    conn.reply(m.chat, '❌ Hubo un error al realizar la descarga. Puede que el vídeo sea muy pesado intente con . ytmp4doc.', m, rcanal);
+  }
+}
 
-handler.help = ['ytmp4'];
-handler.command = /^(ytmp4)$/i;
-handler.tags = ['downloader'];  
+handler.help = ['ytmp4 *<url>*'];
+handler.tags = ['download'];
+handler.command = /^ytmp4$/i;
+handler.register = false;
+
 export default handler;
