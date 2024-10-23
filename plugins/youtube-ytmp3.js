5498ec534f6962cc843df244bcb72fe178595589
Oscar66266 <oscarabrahamcabreramarin098@gmail.com>
2024-10-23 09:38:08 -0500

    Update youtube-ytmp3.js

--- plugins/youtube-ytmp3.js
+++ plugins/youtube-ytmp3.js
@@ -1,39 +1,36 @@
-import fetch from 'node-fetch';
 
-let handler = async (m, { conn, args }) => {
-    if (!args[0]) return conn.reply(m.chat, '🚩 Por favor, ingresa un enlace de YouTube.', m);
 
-    await m.react('🕗');
+import axios from 'axios';
 
+let handler = async (m, { conn: star, text, usedPrefix, command }) => {
+    if (!text) return star.reply(m.chat, `Ingresa la URL del video de YouTube.\n\nEjemplo:\n${usedPrefix + command} https://youtu.be/4rDOsvzTicY?si=3Ps-SJyRGzMa83QT`, m);
+
+    await m.react('🕗'); 
+    
     try {
-        let url = `https://widipe.com/download/ytdl?url=${encodeURIComponent(args[0])}&type=mp3`; 
-        let response = await fetch(url);
-        let json = await response.json();
+        const response = await axios.get(`https://api.betabotz.eu.org/api/download/ytmp3?url=${text}&apikey=btzKiyoEditz`);
+        const res = response.data.result;
+        var { mp3, title } = res;
 
-        if (json.status && json.result && json.result.mp3) {
-            let { title, mp3 } = json.result;
+        
+        await star.sendMessage(m.chat, {
+            audio: { url: mp3 },
+            mimetype: 'audio/mpeg',
+            fileName: `${title}.mp3`
+        }, { quoted: m });
 
-            
-            await conn.sendMessage(m.chat, {
-                audio: { url: mp3 },
-                mimetype: 'audio/mpeg',
-                fileName: `${title}.mp3`
-            }, { quoted: m });
+        await m.react('✅'); 
 
-            await m.react('✅');
-        } else {
-            await conn.reply(m.chat, '🚩 No se pudo obtener el archivo de audio MP3.', m);
-            await m.react('❌');
-        }
-    } catch (error) {
-        console.error(error);
-        await conn.reply(m.chat, '🚩 Ocurrió un error al procesar tu solicitud.', m);
-        await m.react('❌');
+    } catch (e) {
+        await m.react('❌'); 
+        star.reply(m.chat, 'Hubo un error al procesar tu solicitud. Verifica que el enlace de YouTube sea válido.', m);
+        console.log(e);
     }
 };
 
 handler.help = ['ytmp3'];
 handler.command = /^(ytmp3)$/i;
 handler.tags = ['downloader'];
+handler.limit = false;
 
 export default handler;
